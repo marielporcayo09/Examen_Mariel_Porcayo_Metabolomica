7e333951b4f12b81a7186a979c5245fa4c21c907
marielporcayo09 <165519518+marielporcayo09@users.noreply.github.com>
2024-05-16 12:18:11 -0600

    EJERCICIO 2 DEL EXAMEN METABOLOMICA MARIEL

---
+++ EJERCICIO 2 EXAMEN METABOLÓMICA MARIEL PORCAYO GONZÁLEZ.R
@@ -0,0 +1,12 @@
+
+#Creador: Mariel Porcayo
+#Analisis de datos de tiempo real
+
+install.packages('pacman') #pacman sirve para llamar e instalar otros paquetes
+
+library(pacman) #library es para ejecutar
+
+p_load('readr', #para llamar a las bases de datos
+       'dplyr') #para facilitar el manejo de datos
+
+##################################################
